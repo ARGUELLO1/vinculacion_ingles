3426dffd3c5cdabb699cda25291c5a3aac6e91a9
SlayerKing <alonsosalinas511@gmail.com>
2025-10-30 17:32:56 -0600

    Estilos de los botones de login y registro

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,26 +1,20 @@
-<nav class="-mx-3 flex flex-1 justify-end">
+<nav class="flex flex-1 justify-end">
     @auth
-        <a
-            href="{{ url('/dashboard') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-        >
+        <a href="{{ url('/dashboard') }}"
+            class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-gray-500/70 focus:outline-none focus-visible:ring-[#FF2D20]">
             Dashboard
         </a>
     @else
-        <a
-            href="{{ route('login') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-        >
-            Log in
-        </a>
+        <div class="flex items-center gap-2 sm:gap-3">
+            <x-primary-button :href="route('login')" wire:navigate class="text-sm sm:text-base">
+                Login
+            </x-primary-button>
 
-        @if (Route::has('register'))
-            <a
-                href="{{ route('register') }}"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-            >
-                Register
-            </a>
-        @endif
+            @if (Route::has('register'))
+                <x-secondary-button :href="route('register')" wire:navigate class="text-sm sm:text-base">
+                    Registro
+                </x-secondary-button>
+            @endif
+        </div>
     @endauth
 </nav>
