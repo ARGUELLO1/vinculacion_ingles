f7813abe6c82673d9b0412b2dbf71240e95355be
DAVID DANIEL ARGUELLO CHAVEZ <david202119013@ixtapaluca.tecnm.mx>
2025-11-29 16:41:28 -0600

    Arreglo responsibe de cartas de termino

--- resources/views/livewire/alumno/carterm.blade.php
+++ resources/views/livewire/alumno/carterm.blade.php
@@ -8,35 +8,37 @@
         @if (!$carta_documento)
             <h1>AQUI SE MOSTRARAN LAS CARTAS DE TERMINO CUANDO SE SUBAN AL SISTEMA</h1>
         @else
-        
-        <h1 class="text-red-500 my-3 lg:text-xl">*Se muestran las constancias que no han sido asignadas a su alumno correspondiente por lo que no se guardaran a largo plazo.</h1>
-            <table
-                class="w-full text-center border border-separate rounded-lg table-auto border-gray-400 bg-gray-100 text-md">
-                <tr>
-                    <th class="bg-blue-800 text-white" colspan="2">CONSTANCIAS DISPONIBLES</th>
-                </tr>
-                <tr>
-                    <td>NOMBRE DEL DOCUMENTO</th>
-                    <td>OPCIÓN</th>
-                </tr>
-
-                @foreach ($archivos as $archivo)
+            <h1 class="text-red-500 my-3 lg:text-xl">*Se muestran las constancias que no han sido asignadas a su alumno
+                correspondiente por lo que no se guardaran a largo plazo.</h1>
+            <div class="overflow-x-auto">
+                <table
+                    class="overflow-x-auto w-full text-center border border-separate rounded-lg table-auto border-gray-400 bg-gray-100 text-md">
                     <tr>
-                        <td class="bg-white border border-white rounded-lg">
-                            <p>{{ basename($archivo) }}</p>
-                        </td>
-                        <td class="bg-white border border-white rounded-lg hover:bg-blue-800 hover:text-white">
-                            <a href="{{ route('Alumno.documento.descargar', [
-                                'archivo' => base64_encode($archivo),
-                            ]) }}"
-                                target="_blank">
-                                VER
-                            </a>
-                        </td>
+                        <th class="bg-blue-800 text-white" colspan="2">CONSTANCIAS DISPONIBLES</th>
                     </tr>
-                @endforeach
+                    <tr>
+                        <td>NOMBRE DEL DOCUMENTO</th>
+                        <td>OPCIÓN</th>
+                    </tr>
 
-            </table>
+                    @foreach ($archivos as $archivo)
+                        <tr>
+                            <td class="bg-white border border-white rounded-lg">
+                                <p>{{ basename($archivo) }}</p>
+                            </td>
+                            <td class="bg-white border border-white rounded-lg hover:bg-blue-800 hover:text-white">
+                                <a href="{{ route('Alumno.documento.descargar', [
+                                    'archivo' => base64_encode($archivo),
+                                ]) }}"
+                                    target="_blank">
+                                    VER
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </table>
+            </div>
     </div>
     @endif
 </div>
